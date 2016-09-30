d9460ff72d53d1e2096b42323b81286246154a7b
Max Harris <mjharris@colorado.edu>
2016-09-30 17:58:52 -0600

    first commit - webpack dev, prod, test angular2 skeleton

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'main-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
