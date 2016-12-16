b695e89b1a93cab05a601f459c1a41a95a584739
Max Harris <mjharris@colorado.edu>
2016-12-15 20:08:11 -0600

    tiny bit of login polish

--- client/src/modules/auth/login.page.ts
+++ client/src/modules/auth/login.page.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { SessionActions } from '../../actions/session.actions';
 import { UserActions } from '../../actions/user.actions';
 import * as userReducer from '../../reducers/user.reducer';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { User } from '../../types';
 
 @Component({
   template: `
     <button (click)="loggingIn = false" *ngIf="loggingIn">Sign Up!</button>
     <button (click)="loggingIn = true" *ngIf="!loggingIn">I already have an account</button>
     <form (submit)="onSubmit()">
-      <input type="text" name="email" [(ngModel)]="user.email" placeholder="Email"/>
-      <input type="password" name="password" [(ngModel)]="user.password" placeholder="Password"/>
-      <input type="password" name="passwordConfirmation" [(ngModel)]="user.passwordConfirmation" placeholder="Confirm password" *ngIf="!loggingIn"/>
+      <input type="text" name="email" [(ngModel)]="userForm.email" placeholder="Email"/>
+      <input type="password" name="password" [(ngModel)]="userForm.password" placeholder="Password"/>
+      <input type="password" name="passwordConfirmation" [(ngModel)]="userForm.passwordConfirmation" placeholder="Confirm password" *ngIf="!loggingIn"/>
       <button type="submit">Login</button>
     </form>
   `
 })
-export class LoginPage implements OnInit, OnDestroy {
+export class LoginPage implements OnDestroy {
   private loggingIn: boolean = true;
   private sub: Subscription;
 
-  private user = {
+  private userForm = {
     email: '',
     password: '',
     passwordConfirmation: ''
@@ -35,19 +36,21 @@ export class LoginPage implements OnInit, OnDestroy {
     private router: Router
   ) {}
 
-  ngOnInit() {
+  onSubmit(): void {
+    if (this.loggingIn)
+      this.sessionActions.login(this.userForm).then(() => {
+        this.postLogin();
+      });
+    else if (!this.loggingIn) // Signing up
+      this.userActions.signUp(this.userForm).then(() => {
+        this.postLogin();
+      });
+  }
+
+  postLogin() {
     this.sub = userReducer.getCurrentUser(this.store).subscribe(user => {
       if (user) this.router.navigate(['/home']);
     });
-  }
-
-  onSubmit(): void {
-    if (this.loggingIn)
-      this.sessionActions.login(this.user).then(() => {
-
-      });
-    else if (!this.loggingIn) // Signing up
-      this.userActions.signUp(this.user);
   }
 
   ngOnDestroy() {
