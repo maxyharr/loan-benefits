cd42279afe57364e835dff5f0e9e8ee46af19102
Max Harris <mjharris@colorado.edu>
2016-12-23 21:18:52 -0600

    add routes to be recognized through rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   get '/', to: 'client#index'
-  get '/home', to: 'client#index', as: :client_home # /(*page) doesn't work because it matches all the other routes
-  get '/login', to: 'client#index', as: :client_login # /(*page) doesn't work because it matches all the other routes
+  get '/home', to: 'client#index', as: :home # /(*page) doesn't work because it matches all the other routes
+  get '/login', to: 'client#index', as: :login # /(*page) doesn't work because it matches all the other routes
+  get '/signup', to: 'client#index', as: :signup # /(*page) doesn't work because it matches all the other routes
+  get '/styleguide', to: 'client#index', as: :styleguide # /(*page) doesn't work because it matches all the other routes
 
   namespace :api do
     post 'login', to: 'user_sessions#create', as: :login
